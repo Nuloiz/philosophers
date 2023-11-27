1fd733be8d2858b610830b3acfffac464185b152
nschutz <nschutz@student.42wolfsburg.de>
2023-11-27 12:40:12 +0100

    feat: joining threads and destroying mutex when finished

--- philo/src/free.c
+++ philo/src/free.c
@@ -14,6 +14,15 @@
 
 void	free_every(t_info_i input)
 {
+	int i;
+
+	i = 0;
+	while (i < input.count)
+	{
+		pthread_mutex_destroy(&(input.forks[i]));
+		pthread_join((input.thread[i]), NULL);
+		i++;
+	}
 	free(input.philos);
 	free(input.thread);
 	free(input.forks);
